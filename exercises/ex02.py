6d435e503463f81d2b608066d70b5bf92742af68
k0pernicus <dev@carette.xyz>
2025-12-03 01:30:59 +0100

    Bruteforced day 02 part 02

--- exercises/ex02.py
+++ exercises/ex02.py
@@ -1,4 +1,5 @@
 from solver import Solver
+from collections import Counter
 import logger
 import os
 
@@ -37,6 +38,44 @@ class Ex02(Solver):
         return res
 
     def solve_part2(self):
-        lines = self._parse_inputfile()
+        ranges = self._parse_inputfile()
+        res = 0
 
-        return 0
+        interest_list = set()
+
+        self._logger.log("Hum hum... YOLO (2)!!!", logger.DEBUG)
+
+        # collect the interesting values
+        for r in ranges:
+            il, ir = int(r[0]), int(r[1])
+            d = il
+            while d <= ir:
+                sd = str(d)
+                if len(sd) > 1:
+                    c = Counter(sd)
+                    if len(set(c.values())) == 1 or all([x % 2 == 0 for x in c.values()]):
+                        interest_list.add(sd)
+                d += 1
+
+        # find the common stuff
+        for value in interest_list:
+            if len(Counter(value).keys()) == 1:
+                res += int(value)
+                continue
+            d = 1
+            while d <= (len(value) // 2):
+                c = value
+                f, c = c[:d], c[d:]
+                while len(c) > 0:
+                    t = c[:d]
+                    if t == f:
+                        c = c[d:]
+                        continue
+                    else:
+                        break
+                if len(c) == 0:
+                    res += int(value)
+                    break
+                d += 1
+
+        return res
