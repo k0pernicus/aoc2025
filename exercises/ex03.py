810d05205b86e38d265b3e124d925d706427f41d
k0pernicus <dev@carette.xyz>
2025-12-04 22:30:57 +0100

    Solved ex03 part 2

--- exercises/ex03.py
+++ exercises/ex03.py
@@ -36,48 +36,21 @@ class Ex03(Solver):
         s = 0
         lines = self._parse_inputfile()
 
-        MAX_LEN = 11
+        MAX_LEN = 12
 
         for line in lines:
-            # print(f"> current line is {line}")
-            fw = []
-            bl, br = line[:-MAX_LEN], line[-MAX_LEN:]
-            # Find the first highest in bl !
-            lidx = 0
-            ridx = 0
-            hc = 0
-            for i, c in enumerate(bl):
-                if int(c) > hc:
-                    hc = int(c)
-                    lidx = i + 1
-            fw.append(str(hc))
-            # Now that we found the highest, we can compare with the rest of the word
-            while len(fw) < (MAX_LEN + 1):
-                bl = line[lidx:-MAX_LEN-1]
-                br = line[ridx-MAX_LEN:-MAX_LEN++len(fw)] if len(fw) < MAX_LEN else line[-MAX_LEN+ridx:]
-                # print(f"fw is {fw}\nbl is {bl}\nbr is {br}")
-                hc = 0
-                clidx = lidx
-                # find the next highest value
-                for i, c in enumerate(bl):
-                    if int(c) > hc:
-                        hc = int(c)
-                        clidx = i + lidx
-                # print(f">> found highest left value at {hc}, idx {clidx} (ridx is {ridx})")
-                hr = 0
-                cridx = ridx
-                for i, c in enumerate(br):
-                    if int(c) > hr:
-                        hr = int(c)
-                        cridx = i + ridx
-                if hc < hr:
-                    fw.append(str(hr))
-                    ridx = cridx + 1
-                else:
-                    fw.append(str(hc))
-                    lidx = clidx + 1
+            lifo = [line[0]]
+            rest = list(line[:1])
 
-            print(f"FOUND FW WITH VALUE {''.join(fw)}")
-            s += int(''.join(fw))
+            while len(rest) > 0:
+                to_compare = rest.pop(0)
+                if (to_compare < lifo[-1]) and (len(rest) + len(lifo) <= MAX_LEN):
+                    lifo.append(to_compare)
+                    continue
+                while (len(lifo) > 0) and (lifo[-1] < to_compare) and (len(rest) + len(lifo) >= MAX_LEN):
+                    lifo.pop(-1)
+                lifo.append(to_compare)
+
+            s += int(''.join([str(x) for x in lifo[:MAX_LEN]]))
 
         return s
